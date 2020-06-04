b7c0fcc8a1220be56f61baba89deaaf192e6bab1
Mira Williams <mpwms11@gmail.com>
2020-06-04 02:13:01 -0400

    add redirect to Dojo Survey

--- Dojo_Survey/form_app/urls.py
+++ Dojo_Survey/form_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.form_survey),
     path('results', views.results),
+    path('redir_funct', views.redir_funct),
 ]
--- Dojo_Survey/form_app/views.py
+++ Dojo_Survey/form_app/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
 def form_survey(request):
     return render(request, 'form.html')
 
 def results(request):
-    context = {
-        'name': request.POST['name'],
-        'language': request.POST['language'],
-        'location': request.POST['location'],
-        'hobbies': request.POST['hobbies']
-    }
-    return render(request, 'results.html', context)
+    request.session['name'] = request.POST['name']
+    request.session['language'] = request.POST['language']
+    request.session['location'] = request.POST['location']
+    request.session['hobbies'] = request.POST['hobbies']
+    return redirect('/redir_funct')
+
+def redir_funct(request):
+    return render(request, 'results.html')
