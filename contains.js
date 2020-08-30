9ec2eb6a20bc1fc6457031feb5b19c81e483e3fb
Mira Williams <mpwms11@gmail.com>
2020-08-29 21:33:28 -0400

    contains

---
+++ contains.js
@@ -0,0 +1,47 @@
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    addFront(value) {
+        newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+
+        return this;
+    }
+
+    removeFront() {
+        this.head = this.head.next;
+
+        return this;
+    }
+
+    front() {
+        if (this.head) {
+            return this.head.data;
+        }
+        return null;
+    }
+
+    contains(value) {
+        current = this.head
+
+        while(current){
+            if (current.data === value){
+                return true
+            }
+            current = current.next
+        }
+        return false
+    }
+}
+
